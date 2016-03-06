b6b229ecd131230c27a6ab3555945063ffbfe6ba
oomusou <oomusou@gmail.com>
2016-03-06 23:54:46 +0800

    修改UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,24 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests;
+use App\Services\Payment\PaymentEnum;
+use App\Services\PaymentService;
 use App\Services\UserService;
 
 class UserController extends Controller
 {
-    /** @var UserService */
+    /** @var UserService  */
     private $userService;
+    /** @var PaymentService */
+    private $paymentService;
 
     /**
      * UserController constructor.
      * @param UserService $userService
+     * @param PaymentService $paymentService
      */
-    public function __construct(UserService $userService)
+    public function __construct(UserService $userService, PaymentService $paymentService)
     {
         $this->userService = $userService;
+        $this->paymentService = $paymentService;
     }
 
     public function show()
     {
-        $this->userService->show('admin');
+        $this->userService->show('Admin');
+        $this->paymentService->setPayment(PaymentEnum::AliPay);
+        $this->paymentService->checkout(1000);
     }
 }
