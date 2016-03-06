a8624e1185ed77e9480736eb27003ed32ae1e0aa
oomusou <oomusou@gmail.com>
2016-03-06 23:11:52 +0800

    新增PayPal.php

---
+++ app/Services/Payment/PayPal.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services\Payment;
+
+class PayPal implements PaymentInterface
+{
+    /** @var PayPalSDK */
+    private $payPalSDK;
+
+    /**
+     * PayPal constructor.
+     * @param PayPalSDK $payPalSDK
+     */
+    public function __construct(PayPalSDK $payPalSDK)
+    {
+        $this->payPalSDK = $payPalSDK;
+    }
+
+    /**
+     * 使用金流付款
+     *
+     * @param int $amount
+     * @return void
+     */
+    public function checkout(int $amount)
+    {
+        $this->payPalSDK->pay($amount);
+    }
+}
