cb8eda4a462a75185fb8a457f3440abbe730fd8a
oomusou <oomusou@gmail.com>
2016-03-08 10:43:43 +0800

    新增UserService.php

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,13 +3,38 @@
 namespace App\Services;
 
 use App;
+use App\Repositories\UserRepository;
 use App\Services\User\AbstractUser;
+use App\User;
 
 class UserService
 {
+    /** @var UserRepository */
+    private $userRepository;
+
+
+    /**
+     * UserService constructor.
+     * @param UserRepository $userRepository
+     */
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     public function show(string $type)
     {
         App::bind(AbstractUser::class, 'App\Services\User\\' . ucfirst($type));
         App::make(AbstractUser::class)->show();
+    }
+
+    public function getAllUsers()
+    {
+        $users = $this->userRepository->getAllUsers();
+
+        foreach($users as $user) {
+            echo($user->name);
+        }
+
     }
 }
